90f3c94116db6a08c9fe08f7e9be29d2d4390c82
VladislavK <vladislav.kochurko@gmail.com>
2024-02-06 17:45:02 +0300

    feat: integrate postgres db to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,36 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { SequelizeModule } from "@nestjs/sequelize";
+import * as Joi from 'joi';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        dialect: "postgres",
+        host: "localhost",
+        port: configService.get("POSTGRES_DB_PORT"),
+        username: configService.get("POSTGRES_DB_USER"),
+        password: configService.get("POSTGRES_DB_PASS"),
+        database: configService.get("POSTGRES_DB_NAME"),
+        autoLoadModels: true,
+        synchronize: true
+      }),
+      inject: [ConfigService]
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_DB_PASS: Joi.string().required(),
+        POSTGRES_DB_USER: Joi.string().required(),
+        POSTGRES_DB_NAME: Joi.string().required(),
+        POSTGRES_DB_PORT: Joi.number().required()
+      })
+    })
+  ],
+  controllers: [],
+  providers: []
 })
-export class AppModule {}
+export class AppModule {
+}
